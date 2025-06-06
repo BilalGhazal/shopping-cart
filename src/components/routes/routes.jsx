fa70cd33f62b15a7ccae3c7cbe1bcaf27800987b
Bilal <bilalghazal98@gmail.com>
2025-06-06 07:32:31 +0300

    Finish errorpage

--- src/components/routes/routes.jsx
+++ src/components/routes/routes.jsx
@@ -1,9 +1,11 @@
 import App from "../app/App"
+import ErrorPage from "../errorpage/ErrorPage"
 
 const routes = [
     {
         path: "/",
         element: <App />,
+        errorElement: <ErrorPage />,
     }
 ]
 
