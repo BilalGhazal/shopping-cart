4dd0f771b80728c0e3766978ff6bf023d84a1ba6
Bilal <bilalghazal98@gmail.com>
2025-06-06 11:14:47 +0300

    Add counter to shopping cart

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,13 @@
+import { useState } from 'react'
 import './App.css'
 import Navbar from "../navbar/Navbar"
 import Homepage from "../homepage/Homepage"
 
 function App() {
+  const [shoppingCartCounter, setShopingCartCounter] = useState(0)
   return (
     <>
-      <Navbar />
+      <Navbar shoppingCartCounter={shoppingCartCounter} />
       <Homepage />
     </>
     
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import navbar from "./navbar.module.css"
 import { Link, NavLink } from "react-router-dom"
 
-function Navbar() {
+function Navbar({shoppingCartCounter}) {
     return (
         <nav className={navbar.container}>
             <div>
@@ -19,6 +19,7 @@ function Navbar() {
 
             <button type="button" className={navbar.bttn} aria-label="Open shopping cart">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="8" cy="21" r="1"/><circle cx="19" cy="21" r="1"/><path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"/></svg>
+                {shoppingCartCounter > 0 && <span className={navbar.shoppingCartCounter}>{shoppingCartCounter}</span>}
             </button>
         </nav>
     )
